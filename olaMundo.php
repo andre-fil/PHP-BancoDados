f62b339cd67947994e2d9e18d742dfce572a76ba
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-03-30 12:21:07 -0300

    Primeiros códigos - hello world

---
+++ olaMundo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="modelo.css">
+    <title>Iniciando PHP</title>
+    
+</head>
+<body>
+    <head>
+        <h1>php - curso</h1>
+        <div>
+            <?php
+            $nome = 'André';
+            $idade = 20;
+            echo $nome. " tem ". $idade, " anos  <br>";
+            echo "$nome tem $idade anos"
+;            ?>
+        </div>
+    </head>
+</body>
+</html>
