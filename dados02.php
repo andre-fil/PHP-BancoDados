f7898428cdfc769048dbcd1b705a8be787e8561f
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-04-03 12:07:32 -0300

    Formulários e envios de dados

---
+++ dados02.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="modelo.css">
+    <title>Formulários-02</title>
+</head>
+<body>
+    <h1>Formulários com PHP.2</h1>
+    <div>
+        <?php
+            $nome = isset( $_GET['nome'])? $_GET["nome"]:"[não informado]";
+            $nasc = isset($_GET['nasc'])?$_GET['nasc']:0;
+            $sexo = isset($_GET['sexo'])?$_GET['sexo']:'[sem sexo]';
+            $anos = date("Y") - $nasc;
+            echo "O nome recebido é $nome, do sexo $sexo, e tem $anos anos"
+        
+
+        ?>
+         <br/>
+        <a href="formulario02.html"> Voltar</a>
+    </div>
+
+  
+    
+</body>
+</html>
